e47c3d4ad8639f44597b16519bfda9d8af2d91ed
Ben Shope <bshope@uber.com>
2019-01-20 16:05:09 -0800

    minor

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, "./src.js"),
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index.js",
