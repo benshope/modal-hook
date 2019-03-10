c3e4c4d333cd477b851e3f772931438644abf585
Benjamin Shope <nimajnebs@gmail.com>
2019-03-10 06:45:37 -0700

    try this

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    output: {
+        filename: 'index.js',
+    },
+    externals: /^[^.]/,
+}
